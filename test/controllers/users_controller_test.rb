57d23bccc0cf415a41f336f7c8fe1af8a1968d90
vietphxfce <tatsusaki1@gmail.com>
2016-09-17 12:18:28 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = users(:archer)
   end
   
+  test 'should redirect following when not logged in' do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+  
+  test 'should redirect followers when not logged in' do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
+  
   test 'should destroy successfully' do
     log_in_as(@user)
     assert_difference 'User.count', -1 do
